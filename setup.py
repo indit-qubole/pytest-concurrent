7ab8554fd33a07aa5c02b74b07662420566b6330
indit-qubole <47172603+indit-qubole@users.noreply.github.com>
2019-03-20 12:01:43 +0530

    Add files via upload

---
+++ setup.py
@@ -0,0 +1,55 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+import os
+import codecs
+from setuptools import setup
+
+
+def read(fname):
+    file_path = os.path.join(os.path.dirname(__file__), fname)
+    return codecs.open(file_path, encoding='utf-8').read()
+
+
+setup(
+    name='pytest-concurrent',
+    version='0.2.0',
+    author='James Wang, Reverb Chu',
+    author_email='jamesw96@uw.edu, reverbc@me.com',
+    maintainer='James Wang, Reverb Chu',
+    maintainer_email='jamesw96@uw.edu, reverbc@me.com',
+    license='MIT',
+    url='https://github.com/reverbc/pytest-concurrent',
+    description='Concurrently execute test cases with multithread'
+                ', multiprocess and gevent',
+    long_description=read('README.rst'),
+    packages=['pytest_concurrent', 'pytest_concurrent.modes'],
+    install_requires=[
+        'pytest>=3.1.1',
+        'futures;python_version<"3.0"',
+        'psutil>=5.2.2'],
+    classifiers=[
+        'Development Status :: 4 - Beta',
+        'Framework :: Pytest',
+        'Intended Audience :: Developers',
+        'Topic :: Software Development :: Testing',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 2',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
+        'Programming Language :: Python :: Implementation :: CPython',
+        'Operating System :: OS Independent',
+        'License :: OSI Approved :: MIT License',
+    ],
+    entry_points={
+        'pytest11': [
+            'concurrent = pytest_concurrent.plugin',
+        ],
+    },
+    setup_requires=['pytest-runner'],
+    tests_require=['pytest'],
+)
---
+++ tests/test_basic.py
@@ -0,0 +1,57 @@
+# -*- coding: utf-8 -*-
+import sys
+import pytest
+
+
+def test_help_message(testdir):
+    result = testdir.runpytest(
+        '--help',
+    )
+    # fnmatch_lines does an assertion internally
+    result.stdout.fnmatch_lines([
+        'concurrent:',
+        '*--concmode=CONCURRENT_MODE',
+        '*--concworkers=CONCURRENT_WORKERS',
+    ])
+
+
+@pytest.mark.skipif(sys.platform == 'win32',
+                    reason="does not run on windows")
+def test_ini_setting(testdir):
+    '''notice that this is not tested for windows'''
+    concurrent_mode = 'mproc'
+    concurrent_workers = 100
+
+    testdir.makeini("""
+        [pytest]
+        concurrent_mode = %s
+        concurrent_workers = %d
+    """ % (concurrent_mode, concurrent_workers))
+
+    testdir.makepyfile("""
+        import pytest
+
+        @pytest.fixture
+        def concurrent_mode(request):
+            return request.config.getini('concurrent_mode')
+
+        @pytest.fixture
+        def concurrent_workers(request):
+            return request.config.getini('concurrent_workers')
+
+        def test_concurrent_mode(concurrent_mode):
+            assert concurrent_mode == '%s'
+
+        def test_concurrent_workers(concurrent_workers):
+            assert concurrent_workers == '%d'
+    """ % (concurrent_mode, concurrent_workers))
+
+    result = testdir.runpytest('-v')
+
+    # fnmatch_lines does an assertion internally
+    result.stdout.fnmatch_lines([
+        '*= 2 passed in * seconds =*'
+    ])
+
+    # make sure that that we get a '0' exit code for the testsuite
+    assert result.ret == 0
